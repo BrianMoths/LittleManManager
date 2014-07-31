8de18175353a6985807aa6e6ce37052f998b3b4b
brian <brian@brian-desktop>
2014-07-30 22:53:40 -0500

    added documentation

--- src/littlemangame/littlemancommands/LittleManCommander.java
+++ src/littlemangame/littlemancommands/LittleManCommander.java
@@ -4,8 +4,7 @@
  */
 package littlemangame.littlemancommands;
 
-import littlemangame.computer.Computer;
-import littlemangame.genericLittleMan.GenericLittleManCommander;
+import littlemangame.computer.Office;
 import littlemangame.littleman.LittleMan;
 import littlemangame.notebookdeveloper.gui.OfficeView;
 
@@ -15,16 +14,35 @@ import littlemangame.notebookdeveloper.gui.OfficeView;
  */
 public class LittleManCommander extends GenericLittleManCommander<LittleMan> {
 
+    /**
+     * constructs a little man commander which is commanding the given little
+     * man
+     *
+     * @param littleMan the little man to be commanded
+     */
     public LittleManCommander(LittleMan littleMan) {
         super(littleMan);
     }
 
+    /**
+     * constructs a little man commander that commands a little man working in
+     * the office of the given office view
+     *
+     * @param officeView the office view that the commanded little man should
+     * work with
+     */
     public LittleManCommander(OfficeView officeView) {
-        this(new Computer(officeView.getOutputPanel(), officeView.getInputPanel()));
+        this(new Office(officeView.getOutputPanel(), officeView.getInputPanel()));
     }
 
-    public LittleManCommander(Computer computer) {
-        super(new LittleMan(computer));
+    /**
+     * creates a little man commander commanding a new little man to work in the
+     * given office
+     *
+     * @param office the office the commanded little man is to work in
+     */
+    public LittleManCommander(Office office) {
+        super(new LittleMan(office));
     }
 
 }
