ff5c16579783441153efa9fdcd42339db95a4a34
brian <brian@brian-desktop>
2014-08-04 08:47:56 -0500

    added base class for little man gui

---
+++ src/littlemangame/notebookdeveloper/gui/BaseLittleManGui.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package littlemangame.notebookdeveloper.gui;
+
+import littlemangame.GenericLittleManGui;
+
+/**
+ *
+ * @author brian
+ */
+public class BaseLittleManGui extends GenericLittleManGui<BaseNotebookDeveloperGui> {
+
+    public BaseLittleManGui() {
+        super(new BaseNotebookDeveloperGui());
+    }
+
+}
